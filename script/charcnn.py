6a8463eaf1aaf0b915e2bf1223bf70e9fde736c4
RyujiTamaki <e44119rt@gmail.com>
2018-11-11 13:34:43 +0900

    charcnnは無理

---
+++ script/charcnn.py
@@ -0,0 +1,203 @@
+from contextlib import contextmanager
+import gc
+import os
+import time
+import numpy as np
+import pandas as pd
+from keras import backend as K
+from keras import regularizers
+from keras.callbacks import Callback
+from keras.callbacks import EarlyStopping
+from keras.callbacks import ModelCheckpoint
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Embedding
+from keras.layers import SeparableConv1D
+from keras.layers import MaxPooling1D
+from keras.layers import GlobalAveragePooling1D
+from keras.models import Sequential
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
+from sklearn.metrics import f1_score
+from sklearn.model_selection import train_test_split
+from sklearn.utils import class_weight
+
+MAX_SEQUENCE_LENGTH = 1014
+embedding_dim = 70
+
+@contextmanager
+def timer(name):
+    t0 = time.time()
+    yield
+    print(f'[{name}] done in {time.time() - t0:.0f} s')
+
+
+def sepcnn_model(blocks,
+                 filters,
+                 kernel_size,
+                 dropout_rate,
+                 pool_size,
+                 input_shape,
+                 vocab_size):
+    model = Sequential()
+    model.add(Embedding(input_dim=vocab_size + 1,
+                        output_dim=embedding_dim,
+                        input_length=input_shape[0]))
+
+    for _ in range(blocks-1):
+        model.add(Dropout(rate=dropout_rate))
+        model.add(SeparableConv1D(filters=filters,
+                                  kernel_size=kernel_size,
+                                  activation='relu',
+                                  bias_initializer='random_uniform',
+                                  depthwise_initializer='random_uniform',
+                                  padding='same'))
+        model.add(SeparableConv1D(filters=filters,
+                                  kernel_size=kernel_size,
+                                  activation='relu',
+                                  bias_initializer='random_uniform',
+                                  depthwise_initializer='random_uniform',
+                                  padding='same'))
+        model.add(MaxPooling1D(pool_size=pool_size))
+
+    model.add(SeparableConv1D(filters=filters * 2,
+                              kernel_size=kernel_size,
+                              activation='relu',
+                              bias_initializer='random_uniform',
+                              depthwise_initializer='random_uniform',
+                              padding='same'))
+    model.add(SeparableConv1D(filters=filters * 2,
+                              kernel_size=kernel_size,
+                              activation='relu',
+                              bias_initializer='random_uniform',
+                              depthwise_initializer='random_uniform',
+                              padding='same'))
+    model.add(GlobalAveragePooling1D())
+    model.add(Dropout(rate=dropout_rate))
+    model.add(Dense(1, activation='sigmoid'))
+    model.summary()
+    return model
+
+
+def get_best_threshold(y_pred_val,
+                       y_val):
+    threshold_dict = {}
+    for thresh in np.arange(0.1, 0.501, 0.01):
+        thresh = np.round(thresh, 2)
+        threshold_dict[thresh] = f1_score(
+            y_val, (y_pred_val > thresh).astype(int)
+        )
+
+    best_threshold = max(threshold_dict, key=threshold_dict.get)
+    print("best threshold: {}".format(best_threshold))
+    print("best f1 score: {}".format(threshold_dict[best_threshold]))
+    return best_threshold
+
+
+def fit_predict(X_train,
+                X_val,
+                y_train,
+                y_val,
+                X_test,
+                model,
+                batch_size=1024):
+    with timer('fitting'):
+        early_stopping = EarlyStopping(monitor='val_loss', patience=2)
+        model_checkpoint = ModelCheckpoint(
+            'best.h5',
+            save_best_only=True,
+            save_weights_only=True
+        )
+        class_weights = class_weight.compute_class_weight(
+            'balanced',
+            np.unique(y_train),
+            y_train
+        )
+        model.compile(
+            loss='binary_crossentropy',
+            optimizer='adam'
+        )
+
+        model.fit(
+            X_train,
+            y_train,
+            validation_data=(X_val, y_val),
+            epochs=1000,
+            batch_size=batch_size,
+            class_weight=class_weights,
+            callbacks=[early_stopping, model_checkpoint],
+            verbose=2
+        )
+
+    model.load_weights('best.h5')
+    y_pred_val = model.predict(X_val, batch_size=2048)[:, 0]
+
+    with timer('predicting'):
+        y_pred = model.predict(X_test, batch_size=2048)[:, 0]
+
+    get_best_threshold(y_pred_val, y_val)
+    return y_pred, y_pred_val
+
+
+def main():
+    with timer('load data'):
+        train_df = pd.read_csv("../input/train.csv")
+        test_df = pd.read_csv('../input/test.csv')
+        X_train = train_df["question_text"].fillna("_na_").values
+        y_train = train_df["target"].values
+        X_test = test_df["question_text"].fillna("_na_").values
+        qid = test_df["qid"]
+
+    with timer('preprocess'):
+        tokenizer = Tokenizer(char_level=True)
+        tokenizer.fit_on_texts(np.hstack((
+            train_df.question_text.values, test_df.question_text.values))
+        )
+        X_train = tokenizer.texts_to_sequences(train_df.question_text.values)
+        X_test = tokenizer.texts_to_sequences(test_df.question_text.values)
+
+        X_train = pad_sequences(X_train, maxlen=MAX_SEQUENCE_LENGTH)
+        X_test = pad_sequences(X_test, maxlen=MAX_SEQUENCE_LENGTH)
+
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train,
+        y_train,
+        test_size=0.1,
+        random_state=39
+    )
+
+    sepcnn = sepcnn_model(
+        blocks=2,
+        filters=64,
+        kernel_size=3,
+        dropout_rate=0.2,
+        pool_size=3,
+        input_shape=X_train.shape[1:],
+        vocab_size=len(tokenizer.word_index)
+    )
+
+    sepcnn_pred_test, sepcnn_pred_val = fit_predict(
+        X_train=X_train,
+        X_val=X_val,
+        y_train=y_train,
+        y_val=y_val,
+        X_test=X_test,
+        model=sepcnn,
+        batch_size=1024
+    )
+
+    del glove_embedding
+    gc.collect()
+
+    threshold = get_best_threshold(sepcnn_pred_val, y_val)
+    y_pred = (np.array(sepcnn_pred_test) > threshold).astype(np.int)
+
+    submit_df = pd.DataFrame({"qid": qid, "prediction": y_pred})
+    submit_df.to_csv(
+        "submission.csv",
+        index=False
+    )
+
+
+if __name__ == '__main__':
+    main()
