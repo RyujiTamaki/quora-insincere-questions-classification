4c8ae75e948bd2a0851a4a93cffaaada1f69cbea
RyujiTamaki <e44119rt@gmail.com>
2019-01-23 12:47:23 +0900

    cnn pytorch

---
+++ script/fork-of-cnn-pytorch.py
@@ -0,0 +1,412 @@
+# standard imports
+import random
+import os
+import time
+import numpy as np
+import pandas as pd
+
+# pytorch imports
+import torch
+import torch.nn as nn
+import torch.utils.data
+
+# imports for preprocessing the questions
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
+# cross validation and metrics
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import roc_curve, precision_recall_curve, f1_score
+
+# progress bars
+from tqdm import tqdm
+tqdm.pandas()
+
+
+embed_size = 300  # how big is each word vector
+max_features = 95000  # how many unique words to use (i.e num rows in embedding vector)
+maxlen = 70  # max number of words in a question to use
+batch_size = 512  # how many samples to process at once
+n_epochs = 5  # how many times to iterate over all samples
+
+enable_local_test = True
+
+class CyclicLR(object):
+    def __init__(self, optimizer, base_lr=1e-3, max_lr=6e-3,
+                 step_size=2000, factor=0.6, min_lr=1e-4, mode='triangular', gamma=1.,
+                 scale_fn=None, scale_mode='cycle', last_batch_iteration=-1):
+
+        if not isinstance(optimizer, torch.optim.Optimizer):
+            raise TypeError('{} is not an Optimizer'.format(
+                type(optimizer).__name__))
+        self.optimizer = optimizer
+
+        if isinstance(base_lr, list) or isinstance(base_lr, tuple):
+            if len(base_lr) != len(optimizer.param_groups):
+                raise ValueError("expected {} base_lr, got {}".format(
+                    len(optimizer.param_groups), len(base_lr)))
+            self.base_lrs = list(base_lr)
+        else:
+            self.base_lrs = [base_lr] * len(optimizer.param_groups)
+
+        if isinstance(max_lr, list) or isinstance(max_lr, tuple):
+            if len(max_lr) != len(optimizer.param_groups):
+                raise ValueError("expected {} max_lr, got {}".format(
+                    len(optimizer.param_groups), len(max_lr)))
+            self.max_lrs = list(max_lr)
+        else:
+            self.max_lrs = [max_lr] * len(optimizer.param_groups)
+
+        self.step_size = step_size
+
+        if mode not in ['triangular', 'triangular2', 'exp_range'] \
+                and scale_fn is None:
+            raise ValueError('mode is invalid and scale_fn is None')
+
+        self.mode = mode
+        self.gamma = gamma
+
+        if scale_fn is None:
+            if self.mode == 'triangular':
+                self.scale_fn = self._triangular_scale_fn
+                self.scale_mode = 'cycle'
+            elif self.mode == 'triangular2':
+                self.scale_fn = self._triangular2_scale_fn
+                self.scale_mode = 'cycle'
+            elif self.mode == 'exp_range':
+                self.scale_fn = self._exp_range_scale_fn
+                self.scale_mode = 'iterations'
+        else:
+            self.scale_fn = scale_fn
+            self.scale_mode = scale_mode
+
+        self.batch_step(last_batch_iteration + 1)
+        self.last_batch_iteration = last_batch_iteration
+
+        self.last_loss = np.inf
+        self.min_lr = min_lr
+        self.factor = factor
+
+    def batch_step(self, batch_iteration=None):
+        if batch_iteration is None:
+            batch_iteration = self.last_batch_iteration + 1
+        self.last_batch_iteration = batch_iteration
+        for param_group, lr in zip(self.optimizer.param_groups, self.get_lr()):
+            param_group['lr'] = lr
+
+    def step(self, loss):
+        if loss > self.last_loss:
+            self.base_lrs = [max(lr * self.factor, self.min_lr) for lr in self.base_lrs]
+            self.max_lrs = [max(lr * self.factor, self.min_lr) for lr in self.max_lrs]
+
+    def _triangular_scale_fn(self, x):
+        return 1.
+
+    def _triangular2_scale_fn(self, x):
+        return 1 / (2. ** (x - 1))
+
+    def _exp_range_scale_fn(self, x):
+        return self.gamma**(x)
+
+    def get_lr(self):
+        step_size = float(self.step_size)
+        cycle = np.floor(1 + self.last_batch_iteration / (2 * step_size))
+        x = np.abs(self.last_batch_iteration / step_size - 2 * cycle + 1)
+
+        lrs = []
+        param_lrs = zip(self.optimizer.param_groups, self.base_lrs, self.max_lrs)
+        for param_group, base_lr, max_lr in param_lrs:
+            base_height = (max_lr - base_lr) * np.maximum(0, (1 - x))
+            if self.scale_mode == 'cycle':
+                lr = base_lr + base_height * self.scale_fn(cycle)
+            else:
+                lr = base_lr + base_height * self.scale_fn(self.last_batch_iteration)
+            lrs.append(lr)
+        return lrs
+
+
+class NeuralNet(nn.Module):
+    def __init__(self,
+                 dropout_rate=0.1,
+                 kernel_num=40,
+                 fixed_length=70,
+                 kernel_size=[3, 4, 5],
+                 static=True):
+        super(NeuralNet, self).__init__()
+
+        self.embedding = nn.Embedding(max_features, embed_size)
+        self.embedding.weight = nn.Parameter(torch.tensor(embedding_matrix, dtype=torch.float32))
+        if static:
+            self.embedding.weight.requires_grad = False
+
+        self.embedding_dropout = nn.Dropout2d(dropout_rate)
+        self.linear = nn.Linear(embed_size, embed_size)
+        
+        self.conv = nn.ModuleList([nn.Conv2d(1, kernel_num, (i, self.embedding.embedding_dim)) for i in kernel_size])
+        self.maxpools = [nn.MaxPool2d((fixed_length+1-i,1)) for i in kernel_size]
+        self.fc = nn.Linear(len(kernel_size)*kernel_num, 1)
+
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(dropout_rate)
+
+    def forward(self, x):
+        x = self.embedding(x).unsqueeze(1)
+        x = self.embedding_dropout(self.relu(self.linear(x)))
+        x = [self.maxpools[i](torch.tanh(cov(x))).squeeze(3).squeeze(2) for i, cov in enumerate(self.conv)]  # B X Kn
+        x = torch.cat(x, dim=1)  # B X Kn * len(Kz)
+        y = self.fc(self.dropout(x))
+
+        return y
+
+
+def seed_torch(seed=1234):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+
+def threshold_search(y_true, y_proba):
+    args = np.argsort(y_proba)
+    tp = y_true.sum()
+    fs = (tp - np.cumsum(y_true[args[:-1]])) / np.arange(y_true.shape[0] + tp - 1, tp, -1)
+    res_idx = np.argmax(fs)
+    best_th = (y_proba[args[res_idx]] + y_proba[args[res_idx + 1]]) / 2
+    best_score = 2 * fs[res_idx]
+    search_result = {'threshold': best_th, 'f1': best_score}
+    return search_result
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def clean_text(x, maxlen=None):
+    puncts = [
+        ',', '.', '"', ':', ')', '(', '-', '!', '?', '|', ';', "'", '$', '&', '/', '[', ']', '>', '%', '=', '#', '*', '+', '\\', '•', '~', '@', '£',
+        '·', '_', '{', '}', '©', '^', '®', '`', '<', '→', '°', '€', '™', '›',  '♥', '←', '×', '§', '″', '′', 'Â', '█', '½', 'à', '…',
+        '“', '★', '”', '–', '●', 'â', '►', '−', '¢', '²', '¬', '░', '¶', '↑', '±', '¿', '▾', '═', '¦', '║', '―', '¥', '▓', '—', '‹', '─',
+        '▒', '：', '¼', '⊕', '▼', '▪', '†', '■', '’', '▀', '¨', '▄', '♫', '☆', 'é', '¯', '♦', '¤', '▲', 'è', '¸', '¾', 'Ã', '⋅', '‘', '∞',
+        '∙', '）', '↓', '、', '│', '（', '»', '，', '♪', '╩', '╚', '³', '・', '╦', '╣', '╔', '╗', '▬', '❤', 'ï', 'Ø', '¹', '≤', '‡', '√',
+    ]
+    x = x.lower()
+    for punct in puncts[:maxlen]:
+        if punct in x:  # add this line
+            x = x.replace(punct, f' {punct} ')
+    return x
+
+
+def load_glove(word_index, max_words=200000, embed_size=300):
+    EMBEDDING_FILE = '../input/embeddings/glove.840B.300d/glove.840B.300d.txt'
+    emb_mean, emb_std = -0.005838499, 0.48782197
+
+    embedding_matrix = np.random.normal(emb_mean, emb_std, (max_words, embed_size))
+    with open(EMBEDDING_FILE, 'r', encoding="utf8") as f:
+        for line in f:
+            word, vec = line.split(' ', 1)
+            if word not in word_index:
+                continue
+            i = word_index[word]
+            if i >= max_words:
+                continue
+            embedding_vector = np.asarray(vec.split(' '), dtype='float32')[:300]
+            if len(embedding_vector) == 300:
+                embedding_matrix[i] = embedding_vector
+    return embedding_matrix
+
+
+def load_para(word_index, max_words=200000, embed_size=300):
+    EMBEDDING_FILE = '../input/embeddings/paragram_300_sl999/paragram_300_sl999.txt'
+    emb_mean, emb_std = -0.0053247833, 0.49346462
+
+    embedding_matrix = np.random.normal(emb_mean, emb_std, (max_words, embed_size))
+    with open(EMBEDDING_FILE, 'r', encoding="utf8", errors='ignore') as f:
+        for line in f:
+            word, vec = line.split(' ', 1)
+            if word not in word_index:
+                continue
+            i = word_index[word]
+            if i >= max_words:
+                continue
+            embedding_vector = np.asarray(vec.split(' '), dtype='float32')[:300]
+            if len(embedding_vector) == 300:
+                embedding_matrix[i] = embedding_vector
+
+    return embedding_matrix
+
+
+def train_model(model, x_train, y_train, x_val, y_val, validate=True):
+
+    train = torch.utils.data.TensorDataset(x_train, y_train)
+    valid = torch.utils.data.TensorDataset(x_val, y_val)
+
+    train_loader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=True)
+    valid_loader = torch.utils.data.DataLoader(valid, batch_size=batch_size, shuffle=False)
+
+    optimizer = torch.optim.Adam(model.parameters())
+    step_size = 300
+    scheduler = CyclicLR(optimizer, base_lr=0.001, max_lr=0.003,
+                         step_size=step_size, mode='exp_range',
+                         gamma=0.99994)
+
+    loss_fn = torch.nn.BCEWithLogitsLoss(reduction='mean').cuda()
+    best_score = -np.inf
+
+    for epoch in range(n_epochs):
+        start_time = time.time()
+        model.train()
+        avg_loss = 0.
+
+        for x_batch, y_batch in tqdm(train_loader, disable=True):
+            y_pred = model(x_batch)
+            scheduler.batch_step()
+            loss = loss_fn(y_pred, y_batch)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            avg_loss += loss.item() / len(train_loader)
+        model.eval()
+        valid_preds = np.zeros((x_val_fold.size(0)))
+
+        if validate:
+            avg_val_loss = 0.
+            for i, (x_batch, y_batch) in enumerate(valid_loader):
+                y_pred = model(x_batch).detach()
+
+                avg_val_loss += loss_fn(y_pred, y_batch).item() / len(valid_loader)
+                valid_preds[i * batch_size:(i+1) * batch_size] = sigmoid(y_pred.cpu().numpy())[:, 0]
+            search_result = threshold_search(y_val.cpu().numpy(), valid_preds)
+
+            val_f1, val_threshold = search_result['f1'], search_result['threshold']
+            elapsed_time = time.time() - start_time
+            print('Epoch {}/{} \t loss={:.4f} \t val_loss={:.4f} \t val_f1={:.4f} best_t={:.2f} \t time={:.2f}s'.format(
+                epoch + 1, n_epochs, avg_loss, avg_val_loss, val_f1, val_threshold, elapsed_time))
+        else:
+            elapsed_time = time.time() - start_time
+            print('Epoch {}/{} \t loss={:.4f} \t time={:.2f}s'.format(
+                epoch + 1, n_epochs, avg_loss, elapsed_time))
+
+    valid_preds = np.zeros((x_val_fold.size(0)))
+
+    avg_val_loss = 0.
+    for i, (x_batch, y_batch) in enumerate(valid_loader):
+        y_pred = model(x_batch).detach()
+
+        avg_val_loss += loss_fn(y_pred, y_batch).item() / len(valid_loader)
+        valid_preds[i * batch_size:(i+1) * batch_size] = sigmoid(y_pred.cpu().numpy())[:, 0]
+
+    print('Validation loss: ', avg_val_loss)
+
+    test_preds = np.zeros((len(test_loader.dataset)))
+
+    for i, (x_batch,) in enumerate(test_loader):
+        y_pred = model(x_batch).detach()
+
+        test_preds[i * batch_size:(i+1) * batch_size] = sigmoid(y_pred.cpu().numpy())[:, 0]
+
+    test_preds_local = np.zeros((len(test_local_loader.dataset)))
+
+    for i, (x_batch,) in enumerate(test_local_loader):
+        y_pred = model(x_batch).detach()
+
+        test_preds_local[i * batch_size:(i+1) * batch_size] = sigmoid(y_pred.cpu().numpy())[:, 0]
+
+    return valid_preds, test_preds, test_preds_local
+
+
+seed_torch()
+
+train_df = pd.read_csv("../input/train.csv")
+test_df = pd.read_csv("../input/test.csv")
+
+if enable_local_test:
+    n_test = len(test_df) * 4
+    train_df, local_test_df = (train_df.iloc[:-n_test].reset_index(drop=True),
+                               train_df.iloc[-n_test:].reset_index(drop=True))
+else:
+    local_test_df = pd.DataFrame([[None, "", 0], [None, "", 0]], columns=['qid', 'question_text', 'target'])
+    n_test = 2
+
+for df in [train_df, test_df, local_test_df]:
+    df["question_text"] = df["question_text"].str.lower()
+    df["question_text"] = df["question_text"].apply(lambda x: clean_text(x))
+    df["question_text"].fillna("_##_", inplace=True)
+
+x_train = train_df["question_text"].values
+x_test = test_df["question_text"].values
+x_test_local = local_test_df["question_text"].values
+
+tokenizer = Tokenizer(num_words=max_features)
+tokenizer.fit_on_texts(list(x_train) + list(x_test_local))
+x_train = tokenizer.texts_to_sequences(x_train)
+x_test = tokenizer.texts_to_sequences(x_test)
+x_test_local = tokenizer.texts_to_sequences(x_test_local)
+
+x_train = pad_sequences(x_train, maxlen=maxlen)
+x_test = pad_sequences(x_test, maxlen=maxlen)
+x_test_local = pad_sequences(x_test_local, maxlen=maxlen)
+
+y_train = train_df['target'].values
+y_test = local_test_df['target'].values
+
+glove_embeddings = load_glove(tokenizer.word_index, len(tokenizer.word_index) + 1)
+paragram_embeddings = load_para(tokenizer.word_index, len(tokenizer.word_index) + 1)
+
+embedding_matrix = np.mean([glove_embeddings, paragram_embeddings], axis=0)
+np.shape(embedding_matrix)
+
+splits = list(StratifiedKFold(n_splits=5, shuffle=True, random_state=10).split(x_train, y_train))
+train_preds = np.zeros((len(train_df)))
+test_preds = np.zeros((len(test_df)))
+
+x_test_cuda = torch.tensor(x_test, dtype=torch.long).cuda()
+test = torch.utils.data.TensorDataset(x_test_cuda)
+test_loader = torch.utils.data.DataLoader(test, batch_size=batch_size, shuffle=False)
+
+x_test_local_cuda = torch.tensor(x_test_local, dtype=torch.long).cuda()
+test_local = torch.utils.data.TensorDataset(x_test_local_cuda)
+test_local_loader = torch.utils.data.DataLoader(test_local, batch_size=batch_size, shuffle=False)
+
+train_preds = np.zeros(len(train_df))
+test_preds = np.zeros((len(test_df), len(splits)))
+test_preds_local = np.zeros((n_test, len(splits)))
+
+seed = 6017
+
+for i, (train_idx, valid_idx) in enumerate(splits):
+    x_train_fold = torch.tensor(x_train[train_idx], dtype=torch.long).cuda()
+    y_train_fold = torch.tensor(y_train[train_idx, np.newaxis], dtype=torch.float32).cuda()
+    x_val_fold = torch.tensor(x_train[valid_idx], dtype=torch.long).cuda()
+    y_val_fold = torch.tensor(y_train[valid_idx, np.newaxis], dtype=torch.float32).cuda()
+
+    print(f'Fold {i + 1}')
+
+    seed_torch(seed + i)
+    model = NeuralNet()
+    model.cuda()
+
+    valid_preds_fold, test_preds_fold, test_preds_local_fold = train_model(
+        model,
+        x_train_fold,
+        y_train_fold,
+        x_val_fold,
+        y_val_fold,
+        validate=True
+    )
+
+    train_preds[valid_idx] = valid_preds_fold
+    test_preds[:, i] = test_preds_fold
+    test_preds_local[:, i] = test_preds_local_fold
+
+search_result = threshold_search(y_train, train_preds)
+print("search result: {}".format(search_result))
+print("pd.DataFrame(test_preds_local).corr()")
+print(pd.DataFrame(test_preds_local).corr())
+print("f1_score(y_test, test_preds_local.mean(axis=1) > search_result['threshold'])")
+print(f1_score(y_test, test_preds_local.mean(axis=1) > search_result['threshold']))
+
+submission = test_df[['qid']].copy()
+submission['prediction'] = test_preds.mean(axis=1) > search_result['threshold']
+submission.to_csv('submission.csv', index=False)
