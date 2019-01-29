894c97b13cea010601d1ebceccb61c6e3025489d
RyujiTamaki <e44119rt@gmail.com>
2019-01-29 14:46:24 +0900

    -

--- script/validation-test.py
+++ script/validation-test.py
@@ -1,5 +1,6 @@
 # standard imports
 import random
+import re
 import os
 import time
 import numpy as np
@@ -253,6 +254,32 @@ def clean_text(x, maxlen=None):
     return x
 
 
+def clean_numbers(x):
+    if bool(re.search(r'\d', x)):
+        x = re.sub('[0-9]{5,}', '#####', x)
+        x = re.sub('[0-9]{4}', '####', x)
+        x = re.sub('[0-9]{3}', '###', x)
+        x = re.sub('[0-9]{2}', '##', x)
+    return x
+
+
+mispell_dict = {"ain't": "is not", "aren't": "are not","can't": "cannot", "'cause": "because", "could've": "could have", "couldn't": "could not", "didn't": "did not",  "doesn't": "does not", "don't": "do not", "hadn't": "had not", "hasn't": "has not", "haven't": "have not", "he'd": "he would","he'll": "he will", "he's": "he is", "how'd": "how did", "how'd'y": "how do you", "how'll": "how will", "how's": "how is",  "I'd": "I would", "I'd've": "I would have", "I'll": "I will", "I'll've": "I will have","I'm": "I am", "I've": "I have", "i'd": "i would", "i'd've": "i would have", "i'll": "i will",  "i'll've": "i will have","i'm": "i am", "i've": "i have", "isn't": "is not", "it'd": "it would", "it'd've": "it would have", "it'll": "it will", "it'll've": "it will have","it's": "it is", "let's": "let us", "ma'am": "madam", "mayn't": "may not", "might've": "might have","mightn't": "might not","mightn't've": "might not have", "must've": "must have", "mustn't": "must not", "mustn't've": "must not have", "needn't": "need not", "needn't've": "need not have","o'clock": "of the clock", "oughtn't": "ought not", "oughtn't've": "ought not have", "shan't": "shall not", "sha'n't": "shall not", "shan't've": "shall not have", "she'd": "she would", "she'd've": "she would have", "she'll": "she will", "she'll've": "she will have", "she's": "she is", "should've": "should have", "shouldn't": "should not", "shouldn't've": "should not have", "so've": "so have","so's": "so as", "this's": "this is","that'd": "that would", "that'd've": "that would have", "that's": "that is", "there'd": "there would", "there'd've": "there would have", "there's": "there is", "here's": "here is","they'd": "they would", "they'd've": "they would have", "they'll": "they will", "they'll've": "they will have", "they're": "they are", "they've": "they have", "to've": "to have", "wasn't": "was not", "we'd": "we would", "we'd've": "we would have", "we'll": "we will", "we'll've": "we will have", "we're": "we are", "we've": "we have", "weren't": "were not", "what'll": "what will", "what'll've": "what will have", "what're": "what are",  "what's": "what is", "what've": "what have", "when's": "when is", "when've": "when have", "where'd": "where did", "where's": "where is", "where've": "where have", "who'll": "who will", "who'll've": "who will have", "who's": "who is", "who've": "who have", "why's": "why is", "why've": "why have", "will've": "will have", "won't": "will not", "won't've": "will not have", "would've": "would have", "wouldn't": "would not", "wouldn't've": "would not have", "y'all": "you all", "y'all'd": "you all would","y'all'd've": "you all would have","y'all're": "you all are","y'all've": "you all have","you'd": "you would", "you'd've": "you would have", "you'll": "you will", "you'll've": "you will have", "you're": "you are", "you've": "you have", 'colour': 'color', 'centre': 'center', 'favourite': 'favorite', 'travelling': 'traveling', 'counselling': 'counseling', 'theatre': 'theater', 'cancelled': 'canceled', 'labour': 'labor', 'organisation': 'organization', 'wwii': 'world war 2', 'citicise': 'criticize', 'youtu ': 'youtube ', 'Qoura': 'Quora', 'sallary': 'salary', 'Whta': 'What', 'narcisist': 'narcissist', 'howdo': 'how do', 'whatare': 'what are', 'howcan': 'how can', 'howmuch': 'how much', 'howmany': 'how many', 'whydo': 'why do', 'doI': 'do I', 'theBest': 'the best', 'howdoes': 'how does', 'mastrubation': 'masturbation', 'mastrubate': 'masturbate', "mastrubating": 'masturbating', 'pennis': 'penis', 'Etherium': 'Ethereum', 'narcissit': 'narcissist', 'bigdata': 'big data', '2k17': '2017', '2k18': '2018', 'qouta': 'quota', 'exboyfriend': 'ex boyfriend', 'airhostess': 'air hostess', "whst": 'what', 'watsapp': 'whatsapp', 'demonitisation': 'demonetization', 'demonitization': 'demonetization', 'demonetisation': 'demonetization'}
+
+
+def _get_mispell(mispell_dict):
+    mispell_re = re.compile('(%s)' % '|'.join(mispell_dict.keys()))
+    return mispell_dict, mispell_re
+
+
+mispellings, mispellings_re = _get_mispell(mispell_dict)
+
+
+def replace_typical_misspell(text):
+    def replace(match):
+        return mispellings[match.group(0)]
+    return mispellings_re.sub(replace, text)
+
+
 def load_glove(word_index, max_words=200000, embed_size=300):
     EMBEDDING_FILE = '../input/embeddings/glove.840B.300d/glove.840B.300d.txt'
     emb_mean, emb_std = -0.005838499, 0.48782197
@@ -392,6 +419,18 @@ def train_model(model, x_train, y_train, x_val, y_val, validate=True):
     return valid_preds, test_preds, test_preds_local
 
 
+def add_features(df):
+    df['question_text'] = df['question_text'].progress_apply(lambda x:str(x))
+    df['total_length'] = df['question_text'].progress_apply(len)
+    df['capitals'] = df['question_text'].progress_apply(lambda comment: sum(1 for c in comment if c.isupper()))
+    df['caps_vs_length'] = df.progress_apply(lambda row: float(row['capitals'])/float(row['total_length']), axis=1)
+    df['num_words'] = df.question_text.str.count('\S+')
+    df['num_unique_words'] = df['question_text'].progress_apply(lambda comment: len(set(w for w in comment.split())))
+    df['words_vs_unique'] = df['num_unique_words'] / df['num_words']
+
+    return df
+
+
 seed_torch()
 
 train_df = pd.read_csv("../input/train.csv")
@@ -409,6 +448,8 @@ else:
 for df in [train_df, test_df, local_test_df]:
     df["question_text"] = df["question_text"].str.lower()
     df["question_text"] = df["question_text"].apply(lambda x: clean_text(x))
+    # df["question_text"] = df["question_text"].apply(lambda x: clean_numbers(x))
+    # df["question_text"] = df["question_text"].apply(lambda x: replace_typical_misspell(x))
     df["question_text"].fillna("_##_", inplace=True)
 
 x_train = train_df["question_text"].values
@@ -480,10 +521,14 @@ for i, (train_idx, valid_idx) in enumerate(splits):
 
 search_result = threshold_search(y_train, train_preds)
 print("search result: {}".format(search_result))
-print("pd.DataFrame(test_preds_local).corr()")
-print(pd.DataFrame(test_preds_local).corr())
-print("f1_score(y_test, test_preds_local.mean(axis=1) > search_result['threshold'])")
-print(f1_score(y_test, test_preds_local.mean(axis=1) > search_result['threshold']))
+print("valid_preds_local auc: {}".format(roc_auc_score(y_train, train_preds)))
+
+if enable_local_test:
+    print("pd.DataFrame(test_preds_local).corr()")
+    print(pd.DataFrame(test_preds_local).corr())
+    print("f1_score(y_test, test_preds_local.mean(axis=1) > search_result['threshold'])")
+    print(f1_score(y_test, test_preds_local.mean(axis=1) > search_result['threshold']))
+    print("test_preds_local auc: {}".format(roc_auc_score(y_test, test_preds_local.mean(axis=1))))
 
 submission = test_df[['qid']].copy()
 submission['prediction'] = test_preds.mean(axis=1) > search_result['threshold']
